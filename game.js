e0da8a37f3834ad541eadc265b2eb6ebb3d0b0b3
Ayush sharma <dsiayush30@gmail.com>
2022-12-01 20:22:30 +0530

    Final commit

---
+++ game.js
@@ -0,0 +1,87 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started=false;
+var level=0;
+
+$(document).keypress(function(){
+  if(!started){
+    $("#level-title").text("Level "+level);
+    nextSequence();
+    started=true;
+  }
+});
+
+$(".btn").click(function(){
+
+  var userChosenColour = $(this).attr("id");  //id of the button that got clicked.
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);   //sending the last index as parameter
+});
+
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+      console.log("success");
+      if(userClickedPattern.length===gamePattern.length){
+        setTimeout(function(){
+          nextSequence();
+        },1000);
+      }
+    }else{
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function(){
+        $("body").removeClass("game-over");
+      },200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+
+  //Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    // $("#" + randomChosenColour).fadeIn().fadeOut().fadeIn();
+    animatePress(randomChosenColor);
+        playSound(randomChosenColor);
+
+}
+
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  },100);
+}
+
+function startOver() {
+
+  //Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
